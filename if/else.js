a8ab8aee284acfaf7cb2e6a6a34fda6c9261d849
g9d5s <gdsleonel@gmail.com>
2021-08-31 20:48:05 -0500

    Tipos de datos y operadores

---
+++ if/else.js
@@ -0,0 +1,9 @@
+//El tipo de condición más común de todos. Traducida literalmente del inglés, se la podría llamar la estructura si...si no, es decir, si se cumple la condición, haz esto, y sino, haz esto otro.
+
+//La sintaxis se compone de la siguiente forma:
+
+if ( /* Condición a evaluar */ ) {
+	/* Código a ejecutar si la condición retorna true */
+} else {
+	/* Código a ejecutar si la condición retorna false */
+}
---
+++ java.js
@@ -0,0 +1,115 @@
+var=carName="Volvo" 
+
+var=x=50
+
+var=myName="Leonel"
+
+
+//Tipos de Datos 
+ 
+  //Númerios- (entry a float )
+   var edad = 26;
+   var pedo = 65.5;
+   
+
+   //String-Cadena de texto 
+    var nombre = "Leonel";
+    var apellido = "Grados";
+
+    //Arry-Arreglo 
+     var amigos = ["Jesus","Ale","Karla"];
+
+     //Objet -Objeto 
+
+      var objeto = {
+          color: "green";
+          size:34;
+      }
+
+
+      //Boolaen  - true or false 
+
+      var boolaen=true;
+      var boolean = false;
+
+      //Undefined   - NO definida 
+
+      var leonel ;
+
+
+      //null
+
+      var carro =null;
+
+
+      //NaN error de operaciones 
+
+    var texto2 ="Hola como estas "
+                                                      //OPERADORES 
+
+                                                      
+// + Adicion 
+var resultado = 4 + 3; //7
+
+// - Substracción
+var resultado = 4 - 3; // 1
+
+// * Multiplicación
+var resultado = 4 * 3; // 12
+
+// / División 
+var resultado = 4 / 2; // 2
+
+// % Módulo
+var resultado = 4 % 2; // 0 (Residuo de la división)
+
+// ++ Incremento
+var resultado = 4++; // 5
+
+// -- Decremento
+ var resultado = 4--; // 3
+
+//EJEMPLOS
+
+
+// var operacion1 =  4+6;
+// Adición 
+var res1 = 10;
+
+// var operacion2 =  8*2 ;
+// Multiplicación
+var resp = 16;
+
+// var operacion3 =  10/2;
+// División
+var resp2 = 5;
+
+// var operacion4 =  5%2;
+// Módulo
+var resp3 = 1;
+
+// var operacion5 =  6++;
+// Incremento
+var resp4 = 7;
+
+//operqcion y gerarquia 
+
+var problema = (5+3)3;
+
+var a = 5;
+var b = 10;
+var c = 15;
+
+(3 + b) * c / a * 2; // 78
+
+((a + b * c) / 5) * 2; // 62
+
+b / a + 2 * c; // 32
+
+(a + b + c / c) * a; // 80
+
+((3 * a) / c) + a + b + c; // 31  
+
+a - (b + c) * a / 1; // -120
+
+
